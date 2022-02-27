eb8795c931760f2eb9c31f9cbac9ecb1ebde7934
Akkiri <97801771+a-kkiri@users.noreply.github.com>
2022-02-27 19:45:41 +0800

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,136 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const math = require('remark-math');
+const katex = require('rehype-katex');
+
+/** @type {import('@docusaurus/types').Config} */
+
+const config = {
+  title: 'Akkiri的知识库',
+  tagline: '基于Docusaurus',
+  url: 'https://doc.irikka.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+        {
+          docs: {
+            routeBasePath: 'docs',
+            path: 'docs',
+            remarkPlugins: [math],
+            rehypePlugins: [katex],
+            sidebarPath: require.resolve('./sidebars.js'),
+            lastVersion: 'current',
+            onlyIncludeVersions: ['current'],
+          },
+          theme: {
+            customCss: require.resolve('./src/css/custom.css'),
+          },
+        },
+    ],
+  ],
+
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'modeling',
+        path: 'modeling',
+        routeBasePath: 'modeling',
+        remarkPlugins: [math],
+        rehypePlugins: [katex],
+        sidebarPath: require.resolve('./sidebars.js'),
+      }, 
+    ],
+  ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            to: '/docs/home',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {
+            to: '/modeling/home',
+            position: 'left',
+            label: '数学建模',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
